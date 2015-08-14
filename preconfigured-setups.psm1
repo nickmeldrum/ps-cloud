f8e60d32e808d7b3dfef086849e580d9ea44f0a0
Nick Meldrum <nick@nickmeldrum.com>
2015-08-14 17:19:03 +0100

    removes old webhook before setting up github deployment hook

--- preconfigured-setups.psm1
+++ preconfigured-setups.psm1
@@ -56,6 +56,12 @@ Function Setup-SiteWithGithubDeployment {
         azure site appsetting add $appSettings $siteName
     }
 
+# Setup appsettings that kudu will read to know what branch to build and which build config msbuild should use
+    azure site appsetting add "deployment_branch=$($vars.BranchName);SCM_BUILD_ARGS=-p:Configuration=$($vars.BuildConfiguration)" $sitename
+# Setup azure site (kudu) to create a github webhook to trigger a build and deploy on a push to github
+    Delete-GithubWebhook $githubRepo -triggerurlsubstring "$sitename.scm.azurewebsites.net/deploy"
+    azure site deployment github --githubusername $githubUsername --githubpassword $githubPassword --githubrepository "$githubUsername/$githubRepo" $sitename 
+
     if ($vars.ReleaseMode -eq "prod") {
         azure site scale mode --mode shared $sitename
 
@@ -65,11 +71,6 @@ Function Setup-SiteWithGithubDeployment {
 
         Set-HostNamesInDnsimpleAndAzure $hostNames $siteName
     }
-
-# Setup appsettings that kudu will read to know what branch to build and which build config msbuild should use
-    azure site appsetting add "deployment_branch=$($vars.BranchName);SCM_BUILD_ARGS=-p:Configuration=$($vars.BuildConfiguration)" $sitename
-# Setup azure site (kudu) to create a github webhook to trigger a build and deploy on a push to github
-    azure site deployment github --githubusername $githubUsername --githubpassword $githubPassword --githubrepository "$githubUsername/$githubRepo" $sitename 
 }
 
 Function Set-HostNamesInDnsimpleAndAzure {
